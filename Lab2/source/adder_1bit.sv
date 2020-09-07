2befafbf06ea1f9c1e5a0344375906416da4f4f0
Victor Le <le99@purdue.edu>
2020-09-07 18:25:13 -0400

    Full Adder added

---
+++ Lab2/source/adder_1bit.sv
@@ -0,0 +1,35 @@
+// $Id: $
+// File name:   adder_1bit.sv
+// Created:     9/6/2020
+// Author:      Victor Le
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: 1 Bit Full Adder Module
+
+module adder_1bit(
+  input wire a,
+  input wire b,
+  input wire carry_in,
+  output wire sum,
+  output wire carry_out
+);
+
+wire ab;
+
+XOR2X1 A1(.A(a), .B(b), .Y(ab));
+XOR2X1 A2(.A(ab), .B(carry_in), .Y(sum));
+
+wire xc_in; //not cin
+wire abc;
+wire or_ab;
+wire x_a;
+wire abc2;
+
+INVX1 U1(.A(carry_in), .Y(xc_in));
+AND2X1 U2(.A(xc_in), .B(a), .Y(x_a));
+AND2X1 U3(.A(x_a), .B(b), .Y(abc));
+OR2X1 U4(.A(a), .B(b), .Y(or_ab));
+AND2X1 U5(.A(or_ab), .B(carry_in), .Y(abc2));
+OR2X1 U6(.A(abc), .B(abc2), .Y(carry_out));
+
+endmodule
--- Lab2/source/adder_4bit.sv
+++ Lab2/source/adder_4bit.sv
@@ -1,23 +1,27 @@
-// 337 TA Provided Lab 2 8-bit adder wrapper file template
-// This code serves as a template for the 8-bit adder design wrapper file 
-// STUDENT: Replace this message and the above header section with an
-// appropriate header based on your other code files
+// $Id: $
+// File name:   adder_4bit.sv
+// Created:     9/7/2020
+// Author:      Victor Le
+// Lab Section: 337-04
+// Version:     1.0  Initial Design Entry
+// Description: 4 bit full adder
 
-module adder_8bit
+
+module adder_4bit
 (
-	input wire [7:0] a,
-	input wire [7:0] b,
+	input wire [3:0] a,
+	input wire [3:0] b,
 	input wire carry_in,
-	output wire [7:0] sum,
+	output wire [3:0] sum,
 	output wire overflow
 );
 
 	// STUDENT: Fill in the correct port map with parameter override syntax for using your n-bit ripple carry adder design to be an 8-bit ripple carry adder design
-/*wire [3:0] carrys; 
+wire [3:0] carrys; 
 adder_1bit ADD0(.a(a[0]), .b(b[0]), .carry_in(carry_in), .sum(sum[0]), .carry_out(carrys[0]));
 adder_1bit ADD1(.a(a[1]), .b(b[1]), .carry_in(carrys[0]), .sum(sum[1]), .carry_out(carrys[1]));
 adder_1bit ADD2(.a(a[2]), .b(b[2]), .carry_in(carrys[1]), .sum(sum[2]), .carry_out(carrys[2]));
 adder_1bit ADD3(.a(a[3]), .b(b[3]), .carry_in(carrys[2]), .sum(sum[3]), .carry_out(overflow));
-*/
+
 
 endmodule
