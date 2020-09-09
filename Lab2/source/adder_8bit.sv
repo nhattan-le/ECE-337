303b16a57739fa47ff9ac5829e31165912bc531d
Victor Le <le99@purdue.edu>
2020-09-09 01:34:23 -0400

    Final Commit including ripple carry adder

--- Lab2/source/adder_8bit.sv
+++ Lab2/source/adder_8bit.sv
@@ -13,11 +13,7 @@ module adder_8bit
 );
 
 	// STUDENT: Fill in the correct port map with parameter override syntax for using your n-bit ripple carry adder design to be an 8-bit ripple carry adder design
-/*wire [3:0] carrys; 
-adder_1bit ADD0(.a(a[0]), .b(b[0]), .carry_in(carry_in), .sum(sum[0]), .carry_out(carrys[0]));
-adder_1bit ADD1(.a(a[1]), .b(b[1]), .carry_in(carrys[0]), .sum(sum[1]), .carry_out(carrys[1]));
-adder_1bit ADD2(.a(a[2]), .b(b[2]), .carry_in(carrys[1]), .sum(sum[2]), .carry_out(carrys[2]));
-adder_1bit ADD3(.a(a[3]), .b(b[3]), .carry_in(carrys[2]), .sum(sum[3]), .carry_out(overflow));
-*/
+
+adder_nbit #(8) ADD8(.a(a), .b(b), .carry_in(carry_in), .sum(sum), .overflow(overflow));
 
 endmodule
